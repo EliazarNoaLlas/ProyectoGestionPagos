9b2a1ce3d7a3bc568d9ca13464abee5ab5d81b7d
EliazarNoa <193003@unsaac.edu.pe>
2025-02-24 16:36:26 -0500

    Subiendo el backend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:3000/api/clients"; // Asegúrate de esta URL
+const API_URL = import.meta.env.VITE_API_URL + "/clients"; // Usa la URL base desde el .env
 
 // Obtener todos los clientes
 export const getClients = async () => {
--- frontend/src/services/auth.js
+++ frontend/src/services/auth.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:3000/api/auth"; // Asegúrate de esta URL
+const API_URL = import.meta.env.VITE_API_URL + "/auth"; // Usa la URL base desde el .env
 
 
 // registrarse
